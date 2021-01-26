e38f15559076ba87b86d547f2a292f6dd62db4d8
Yiqin Wang <60458271+Elisayiqin@users.noreply.github.com>
2021-01-25 18:06:23 -0700

    Add files via upload

--- Q_04.py
+++ Q_04.py
@@ -1,10 +1,16 @@
-def Q_04(self):
-    # Task 4: Return median (i.e., 50% percentile) of the seventh feature: "'U' frequencies of sequence 2" of
-    # the validation dataset: "dataset/validation.csv"
+def Q_04(self, full_dataset_all_numeric):
+    # Task 4: Given a full_dataset (Pandas dataframe) where all the categorical variables are
+    # already replaced with numeric values, return a list of top 20 highly correlated variables
+    # (with respect to the target variable) as a Pandas dataframe with 2 columns {variable,corr_score}.
+    # The corr_score between a variable x and the target variable y needs to be computed using the
+    # Pearson Correlation Coefficient.
 
-    med = self.validation[6].median()
+    import pandas as pd
+
+    ## YOUR CODE HERE ##
 
 
-    return med
+
+    return pd.DataFrame()
 
 
--- Q_05.py
+++ Q_05.py
@@ -1,28 +1,16 @@
-def confusion_matrix(self,y_actual, y_pred):
-    # Task 5: the function takes two arrays of target variable "y": y_actual and y_pred
-    #    denoting ground truth class labels and predicted class labels for the N samples
-    #    when N is the length of both the arrays.
-    # The function should return a list of 4 metrics: TN, FP, FN, TP (in this order).
-    assert(len(y_actual)==len(y_pred))
-    import numpy as np
-    y_actual = np.array(y_actual)
-    y_pred = np.array(y_pred)
-    TP = sum((y_actual == y_pred) * (y_actual == [1]*len(y_actual)))
-    FP = sum((y_actual != y_pred) * (y_actual == [0]*len(y_actual)))
-    TN = sum((y_actual == y_pred) * (y_actual == [0]*len(y_actual)))
-    FN = sum((y_actual != y_pred) * (y_actual == [1]*len(y_actual)))
+def Q_05(self, full_dataset, columns_to_keep):
+    # Task 5: Given a full_dataset (as Pandas dataframe) having all the 37 columns and certain number of rows
+    # and columns_to_keep (as Pandas Dataframe) having 2 columns {variable_name,corr_score_with_target} similar
+    # to the one you computed in Q_04.
+    # Please return a reduced full_dataset keeping only the columns listed in the columns_to_keep dataframe.
+    #
+    import pandas as pd
 
 
 
-    return [TN,FP, FN, TP]
+    #### YOUR CODE HERE####
 
-# def confusion_matrix(self,TP, TN, FP, FN):
-#     return [TN, FP, FN, TP]
 
-def Q_05(self):
-    # Task 5: This function does nothing, but you need to complete the two functions "confusion_matrix" above
-    #   First "confusion_matrix" takes two arguments: y_actual and y_pred
-    # And the second overloaded version of the function takes 4 arguments: TP, TN, FP, FN
-    pass
+    return pd.DataFrame()
 
 
